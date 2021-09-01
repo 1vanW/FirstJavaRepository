a07f0374971478e60e4c20b3d28536dadae982ac
Ivan Hohryakov <vinevega99@gmail.com>
2021-09-01 11:59:32 +0300

    Добавил шляпу и пальто

--- Java.Git.test/src/testGit.java
+++ Java.Git.test/src/testGit.java
@@ -3,6 +3,14 @@ public class testGit {
         System.out.println("Hello Java Git");
         int x = 0;
 
+        String str = "Шляпа";
+        String str2 = "Пальто";
+
+        x = x + 1;
+
+
+
+
 
 
 
